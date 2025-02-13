ff2d30dd67dcff095356e9f51bba0f4d7c76703a
Pandaluf <juliobarrios0296@gmail.com>
2025-02-13 18:50:00 -0500

    feat: Hide the hamburger when clicking

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,10 +3,11 @@ import './NavBar.css';
 
 function NavBar() {
     const menuRef = useRef<HTMLElement | null>(null); // Referencia al menú
+    const checkRef = useRef<HTMLInputElement | null>(null); // Referencia al checkbox
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 50 ) {
+            if (window.scrollY > 50) {
                 menuRef.current?.classList.add('slidedown');
             } else {
                 menuRef.current?.classList.remove('slidedown');
@@ -17,11 +18,26 @@ function NavBar() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    useEffect(() => {
+        const links = document.querySelectorAll(".headerNav__nav__link");
+        const closeMenu = () => {
+            if (checkRef.current) {
+                checkRef.current.checked = false; // Desmarcar el checkbox
+            }
+        };
+
+        links.forEach(link => link.addEventListener("click", closeMenu));
+
+        return () => {
+            links.forEach(link => link.removeEventListener("click", closeMenu));
+        };
+    }, []);
+
     return (
         <section ref={menuRef} className="headerNav">
             <a href="#" className="headerNav__logo">Contador Público</a>
 
-            <input type="checkbox" id="check" />
+            <input type="checkbox" id="check" ref={checkRef} />
             <label htmlFor="check" className="headerNav__icon">
                 <i className="bx bx-menu" id="menu-icon"></i>
                 <i className="bx bx-x" id="close-icon"></i>
